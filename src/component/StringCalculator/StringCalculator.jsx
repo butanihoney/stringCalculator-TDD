823557cbeec18fcea7d587aa787b3330d5b049dc
butanihoney <honeybutani2000@gmail.com>
2024-10-25 00:44:37 +0530

    improved the UI/UX

--- src/component/StringCalculator/StringCalculator.jsx
+++ src/component/StringCalculator/StringCalculator.jsx
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
-import { add } from '../../utils';
-import './StringCalculator.css'
+import React, { useState } from "react";
+import { add } from "../../utils";
+import "./StringCalculator.css";
 
 const StringCalculator = () => {
-    const [input, setInput] = useState('');
-    const [result, setResult] = useState('');
-    const [error, setError] = useState('');
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState("");
+    const [error, setError] = useState("");
 
     const handleCalculate = () => {
-
         try {
             const sum = add(input);
             setResult(sum);
-            setError('');
+            setError("");
         } catch (error) {
             setError(error.message);
-            setResult('');
+            setResult("");
         }
     };
 
     return (
-        <div className='container'>
+        <div className="container">
             <h1>String Calculator</h1>
-            <textarea
-                className='input'
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                placeholder="Enter numbers"
-            />
-            <button className='button' onClick={handleCalculate}>
-                Calculate
-            </button>
-            {error ? <p className='error'>Error: {error}</p> :
-                <p className='result'>Result: {result}</p>}
+            <div className="input-section">
+                <textarea
+                    className="input"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Enter numbers separated by commas or new lines. Use //[delimiter] for custom delimiters."
+                />
+                <button className="button" onClick={handleCalculate}>
+                    Calculate
+                </button>
+            </div>
+            {error ? (
+                <p className="error">Error: {error}</p>
+            ) : (
+                <p className="result">Result: {result}</p>
+            )}
         </div>
     );
 };
